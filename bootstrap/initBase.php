dca4664b38e752818f73aa55ab97cddff1bf1085
phper <2998658517@qq.com>
2019-09-03 02:16:06 +0000

    修改initBase.php

--- bootstrap/initBase.php
+++ bootstrap/initBase.php
@@ -1,5 +1,4 @@
 <?php
-//define('VIDEO_QINIU','http://cz-qiniu.ciguang.tv');
-define('VIDEO_QINIU','http://cdn-media.jingzong.org');
-define('CZ_QINIU','http://cz-qiniu.ciguang.tv');
-define('MY_QINIU','http://cdn-media.yyii.info');
+define('VIDEO_QINIU','*****');    // 这是存在视频的域名
+define('CZ_QINIU','*****');       //
+define('MY_QINIU','*****');       //
